2e5023f91cddd2bf5abc49acb239491639d2ec89
ba_id <mabrakida@gmail.com>
2025-12-27 15:13:48 +0200

    push

--- neet_code/07_trees/13_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ neet_code/07_trees/13_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -27,16 +27,40 @@ from typing import Optional, List
 from neet_code.classes.binary_tree import BinaryTreeNode, test_binary_tree_function
 
 EXAMPLES = [
-    {"inputs": {"root": BinaryTreeNode.from_list([1,2,3])}, "result": [1,3]},
-    {"inputs": {"root": BinaryTreeNode.from_list([1,2,3,4,5,6,7])}, "result": [1,3,7]},
+    {"inputs": {"preorder": [1,2,3,4], "inorder": [2,1,3,4]}, "result": BinaryTreeNode.from_list([1,2,3,None,None,None,4])},
+    {"inputs": {"preorder": [1], "inorder": [1]}, "result": BinaryTreeNode.from_list([1])},
 ]
 
 COMPLEXITY = """
 """
 
-def build_tree(preorder: List[int], inorder: List[int]) -> Optional[BinaryTreeNode]:
-    pass
 
+def build_tree(preorder: List[int], inorder: List[int]) -> Optional[BinaryTreeNode]:
+    if not preorder or not inorder:
+        return None
+
+    inorder_value_to_idx = {v: i for i, v, in enumerate(inorder)}
+    idx = 0
+
+    def helper(left: int, right: int) -> Optional[BinaryTreeNode]:
+        nonlocal idx
+
+        if left >= right:
+            return None
+
+        # build tree node
+        root_val = preorder[idx]
+        root = BinaryTreeNode(root_val)
+        idx += 1
+
+        # add node left and right
+        right_, left_ = inorder_value_to_idx[root_val], inorder_value_to_idx[root_val] + 1
+        root.left = helper(left, right_)
+        root.right = helper(left_, right)
+
+        return root
+
+    return helper(0, len(inorder))
 
 function = build_tree
 
@@ -48,5 +72,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
